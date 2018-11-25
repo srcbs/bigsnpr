a0c86923109f85edc9e7d905f607ac219d18127a
Florian Privé <florian.prive.21@gmail.com>
2018-11-25 22:32:19 +0100

    better version

--- tmp-tests/test-rcpp-parallel2.cpp
+++ tmp-tests/test-rcpp-parallel2.cpp
@@ -33,11 +33,9 @@ NumericVector parallelVectorSum(Environment BM) {
   std::size_t m = xpBM->ncol();
   SubBMCode256Acc macc(xpBM, seq_len(n) - 1, seq_len(m) - 1, BM["code256"]);
 
-  int grain = ::sqrt(m);
-
   NumericVector res(m);
   Sum sum(macc, 0, res);
-  parallelFor(0, m, sum, grain);
+  parallelFor(0, m, sum);
 
   return res;
 }
@@ -52,16 +50,15 @@ snp <- snp_attachExtdata()
 G <- snp$genotypes
 test0 <- parallelVectorSum(G)
 
-G2 <- big_copy(G, ind.row = rep(rows_along(G), 500))
+G2 <- big_copy(G, ind.row = rep(rows_along(G), 100),
+               ind.col = rep(cols_along(G), 10))
 dim(G2)
 RcppParallel::setThreadOptions(1)
-system.time(test1 <- parallelVectorSum(G2))  # 100 / 3
-testthat::expect_identical(test1, 500 * test0)
+system.time(test1 <- parallelVectorSum(G2))
 RcppParallel::setThreadOptions(2)
-system.time(test2 <- parallelVectorSum(G2))  # 177 / 39
-testthat::expect_identical(test2, 500 * test0)
-RcppParallel::setThreadOptions(4)
-system.time(parallelVectorSum(G2))  # 305 / 44
-RcppParallel::setThreadOptions(10)
-system.time(parallelVectorSum(G2))  # 689 / 53
+system.time(test2 <- parallelVectorSum(G2))
+RcppParallel::setThreadOptions(3)
+system.time(parallelVectorSum(G2))
+# RcppParallel::setThreadOptions(10)
+# system.time(parallelVectorSum(G2))
 */
